16c95f63979aa6c870b32ca6619fe4f911a6a100
Sarfaraz Qadir <sarfarazqadir594@gmail.com>
2025-03-04 21:21:58 +0500

    new

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:bookapp/app/Features/Views/onbording/onbording_screen.dart';
 
-// new
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
